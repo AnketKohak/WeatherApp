245de70fce5875be3aea9732f17d7d8bc4863e88
AnketKohak <anketkohak40@gmail.com>
2024-09-24 21:58:16 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Anket Kohak on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
