7a89bcd955f57d06499cf1a30310a605dd311ba3
AnketKohak <anketkohak40@gmail.com>
2024-09-27 20:09:48 +0530

    52 Refactoring and Displaying Weather on the Screen

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -8,20 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var city: String = ""
+    @State private var isFetchingWeather: Bool = false
+    
+    let geocodingClient = GeoCodingClient()
+    
+    let weatherCleint = WeatherClient()
+    
+    @State private var weather: Weather?
+    private func fetchWeather() async  {
+        do{
+            guard let location = try await geocodingClient.corinateByCity(city) else { return }
+            weather = try await weatherCleint.fetchWeather(location: location)
+        }
+        catch{
+            print(error)
+        }
+    }
     var body: some View {
         VStack {
-            Button("Get the Location"){
-                Task{
-                    print("button is pressed")
-                    let geocodingClient = GeoCodingClient()
-                    let weatherCleint = WeatherClient()
-                    let location = try! await geocodingClient.corinateByCity("Mumbai")
-                    let weather = try! await weatherCleint.fetchWeather(location: location!)
-                    print(weather)
+            TextField("City",text:$city)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    isFetchingWeather = true
+                }.task(id: isFetchingWeather) {
+                    if isFetchingWeather {
+                        await fetchWeather()
+                        isFetchingWeather = false
+                        city = ""
+                    }
                 }
+            if let weather {
+                Text("\(MeasurementFormatter.temperature(value: weather.temp))")
+                    .font(.system(size: 100))
             }
-            
-            
+            Spacer()
         }
         .padding()
     }
---
+++ WeatherApp/Extension/MeasurmentFormatter+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  MeasurmentFormatter+Extension.swift
+//  WeatherApp
+//
+//  Created by Anket Kohak on 27/09/24.
+//
+
+import Foundation
+extension MeasurementFormatter{
+    static func temperature(value:Double)->String{
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = 0
+        let formatter = MeasurementFormatter()
+        formatter.numberFormatter = numberFormatter
+        
+        let temp = Measurement(value: value, unit: UnitTemperature.kelvin)
+        
+        return formatter.string(from: temp)
+    }
+}
